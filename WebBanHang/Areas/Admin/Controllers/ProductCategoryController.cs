d9a86cf37e24a3db15edecee5750a623cb26002a
Le Van Dong <itlvd@outlook.com>
2023-09-04 15:32:40 +0700

    done: product controller

---
+++ WebBanHang/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,57 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHang.Models;
+using WebBanHang.Models.EF;
+
+namespace WebBanHang.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        // GET: Admin/ProductCategory
+        private ApplicationDbContext _dbContext = new ApplicationDbContext();
+        
+        public ActionResult Index(int? page)
+        {
+            var pageSize = 15;
+            page = page ?? 1;
+
+            IEnumerable<ProductCategory> items = _dbContext.ProductCategories.OrderBy(x => x.Id);
+
+            /*if (!string.IsNullOrEmpty(searchText))
+            {
+                items = items.Where(x => x.Alias.Contains(searchText) || x.Title.Contains(searchText));
+            }*/
+
+            var result = items.ToPagedList(Convert.ToInt32(page), pageSize);
+
+
+            ViewBag.PageSize = pageSize;
+            ViewBag.PageIndex = page;
+            return View(result);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(ProductCategory model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreateDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                model.Alias = WebBanHang.Models.Common.Filter.FilterChar(model.Title);
+                _dbContext.ProductCategories.Add(model);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("Add");
+        }
+    }
+}
--- WebBanHang/Areas/Admin/Controllers/ProductsController.cs
+++ WebBanHang/Areas/Admin/Controllers/ProductsController.cs
@@ -27,7 +27,137 @@ namespace WebBanHang.Areas.Admin.Controllers
 
         public ActionResult Add()
         {
+            ViewBag.ProductCategory = new SelectList(_dbContext.ProductCategories.ToList(),"Id","Title");
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Product model, List<string> Images, List<int> rDefault)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Images != null && Images.Count > 0)
+                {
+                    for (int i = 0; i < Images.Count; i++)
+                    {
+                        if (i + 1 == rDefault[0])
+                        {
+                            model.Image = Images[i];
+                            model.ProductImage.Add(new ProductImage
+                            {
+                                ProductId = model.Id,
+                                Image = Images[i],
+                                IsDefault = true
+                            });
+                        }
+                        else
+                        {
+                            model.ProductImage.Add(new ProductImage
+                            {
+                                ProductId = model.Id,
+                                Image = Images[i],
+                                IsDefault = false
+                            });
+                        }
+                    }
+                }
+                model.CreateDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                if (string.IsNullOrEmpty(model.SeoTitle))
+                {
+                    model.SeoTitle = model.Title;
+                }
+                if (string.IsNullOrEmpty(model.Alias))
+                    model.Alias = WebBanHang.Models.Common.Filter.FilterChar(model.Title);
+                _dbContext.Products.Add(model);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProductCategory = new SelectList(_dbContext.ProductCategories.ToList(), "Id", "Title");
+            return View(model);
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            ViewBag.ProductCategory = new SelectList(_dbContext.ProductCategories.ToList(), "Id", "Title");
+            var item = _dbContext.Products.Find(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.ModifiedDate = DateTime.Now;
+                model.Alias = WebBanHang.Models.Common.Filter.FilterChar(model.Title);
+                _dbContext.Products.Attach(model);
+                _dbContext.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = _dbContext.Products.Find(id);
+            if (item != null)
+            {
+                _dbContext.Products.Remove(item);
+                _dbContext.SaveChanges();
+                return Json(new { success = true });
+            }
+
+            return Json(new { success = false });
+        }
+
+        [HttpPost]
+        public ActionResult IsActive(int id)
+        {
+            var item = _dbContext.Products.Find(id);
+            if (item != null)
+            {
+                item.IsActive = !item.IsActive;
+                _dbContext.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                _dbContext.SaveChanges();
+                return Json(new { success = true, isActive = item.IsActive });
+            }
+
+            return Json(new { success = false });
+        }
+        [HttpPost]
+        public ActionResult IsHome(int id)
+        {
+            var item = _dbContext.Products.Find(id);
+            if (item != null)
+            {
+                item.IsHome = !item.IsHome;
+                _dbContext.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                _dbContext.SaveChanges();
+                return Json(new { success = true, IsHome = item.IsHome });
+            }
+
+            return Json(new { success = false });
+        }
+
+        [HttpPost]
+        public ActionResult IsSale(int id)
+        {
+            var item = _dbContext.Products.Find(id);
+            if (item != null)
+            {
+                item.IsSale = !item.IsSale;
+                _dbContext.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                _dbContext.SaveChanges();
+                return Json(new { success = true, IsSale = item.IsSale });
+            }
+
+            return Json(new { success = false });
         }
     }
 }
